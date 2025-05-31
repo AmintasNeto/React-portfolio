23941317ff572be9cc193ebeb96761a2fd7f2287
AmintasNeto <netorubok14@gmail.com>
2025-05-31 15:43:24 -0300

    Update the message sender feature

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
 import Work from './components/work/Work';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => {
   return (
     <>
     <Header/>
+    <ToastContainer
+      position='top-center'
+    />
 
     <main className='main'>
       <Home />
